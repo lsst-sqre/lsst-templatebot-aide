1d79ddfceab7109cc05eba1b56792248851ae0ea
Jonathan Sick <jsick@lsst.org>
2019-03-22 10:17:12 -0700

    Implement the handle_generic_prerender function
    This function is capable of creating the GitHub repo for simple projects
    like LSST Stack packages where the repo name can be determined directly
    from template variables.
    
    The handler creates the GitHub repo and sends a
    templatebot-render_ready topic message containing the URL of the GitHub
    repository.

--- templatebotaide/events/handlers/genericprerender.py
+++ templatebotaide/events/handlers/genericprerender.py
@@ -1,5 +1,78 @@
 __all__ = ('handle_generic_prerender',)
 
+from copy import deepcopy
+import datetime
+import gidgethub
+
+from templatebotaide.github import create_repo
+
 
 async def handle_generic_prerender(*, event, schema, app, logger):
-    logger.info('In handle_generic_prerender', event=event)
+    """Handle a ``templatebot-prerender`` event for a template where the
+    GitHub organization and repository can be determined directly from the
+    template variables.
+
+    Parameters
+    ----------
+    event : `dict`
+        The parsed content of the ``templatebot-prerender`` event's message.
+    schema : `dict`
+        The Avro schema corresponding to the ``event``.
+    app : `aiohttp.web.Application`
+        The app instance.
+    logger
+        A `structlog` logger instance with bound context related to the
+        Kafka event.
+
+    Notes
+    -----
+    Once the handler is finished, it sends a ``templatebot-render_ready``
+    event.
+    """
+    logger.info('In handle_generic_prerender', event_data=event)
+
+    if event['template_name'] == 'stack_package':
+        org_name = event['variables']['github_org']
+        repo_name = event['variables']['package_name']
+    else:
+        # Try to work with a general case where github_org and name are the
+        # cookiecutter variables for the repo's org and name on GitHub.
+        try:
+            org_name = event['variables']['github_org']
+        except KeyError:
+            logger.error('event does not have a variables.github_org key')
+            raise
+        try:
+            repo_name = event['variables']['name']
+        except KeyError:
+            logger.error('event does not have a variables.name key')
+            raise
+
+    try:
+        repo_info = await create_repo(
+            org_name=org_name,
+            repo_name=repo_name,
+            app=app,
+            logger=logger
+        )
+    except gidgethub.GitHubException:
+        logger.exception('Error creating the GitHub repository')
+        # TODO send a threaded Slack message back to the user if appropriate
+        raise
+
+    logger.info('Created repo', repo_info=repo_info)
+
+    # Send a response message to templatebot-render_ready
+    render_ready_message = deepcopy(event)
+    render_ready_message['github_repo'] = repo_info['html_url']
+    render_ready_message['retry_count'] = 0
+    now = datetime.datetime.now(datetime.timezone.utc)
+    render_ready_message['initial_timestamp'] = now
+
+    serializer = app['templatebot-aide/renderreadySerializer']
+    render_ready_data = serializer(render_ready_message)
+
+    producer = app['templatebot-aide/producer']
+    topic_name = app['templatebot-aide/renderreadyTopic']
+    await producer.send_and_wait(topic_name, render_ready_data)
+    logger.info('Sent render_ready message', data=render_ready_message)
