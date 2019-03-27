0af1494eccb73681188abf0bda52936f5391ea3a
Jonathan Sick <jsick@lsst.org>
2019-03-27 15:44:08 -0700

    Implement LTD product creation for technotes
    This uses the LTD Keeper (ltd-keeper.lsst.io) API to register a new
    technote with LSST the Docs.

--- templatebotaide/events/handlers/technotepostrender.py
+++ templatebotaide/events/handlers/technotepostrender.py
@@ -3,6 +3,7 @@
 
 __all__ = ('handle_technote_postrender',)
 
+from templatebotaide.lsstthedocs import register_ltd_product
 from templatebotaide.travisci import (
     activate_travis, sync_travis_account, make_travis_repo_url)
 from templatebotaide.slack import post_message
@@ -13,12 +14,58 @@ async def handle_technote_postrender(*, event, schema, app, logger):
     template.
 
     This handler activates Travis CI for the repository.
+
+    Parameters
+    ----------
+    event : `dict`
+        The parsed content of the ``templatebot-postrender`` event's message.
+    schema : `dict`
+        The Avro schema corresponding to the ``event``.
+    app : `aiohttp.web.Application`
+        The app instance.
+    logger
+        A `structlog` logger instance with bound context related to the
+        Kafka event.
     """
     logger.debug('In handle_technote_postrender', event_data=event)
 
     github_repo_url_parts = event['github_repo'].split('/')
     slug = '/'.join((github_repo_url_parts[-2], github_repo_url_parts[-1]))
     travis_url = make_travis_repo_url(slug)
+
+    ltd_slug = '-'.join((event['variables']['series'].lower(),
+                         event['variables']['serial_number']))
+
+    try:
+        ltd_product = await register_ltd_product(
+            slug=ltd_slug,
+            title=event['variables']['title'],
+            github_repo=event['github_repo'],
+            app=app,
+            logger=logger)
+        if event['slack_username'] is not None:
+            await post_message(
+                text="I've set up the technote on _LSST the Docs._ Your "
+                     f"document will appear at {ltd_product['published_url']}",
+                channel=event['slack_channel'],
+                thread_ts=event['slack_thread_ts'],
+                logger=logger,
+                app=app
+            )
+    except Exception:
+        logger.exception(
+            'Failed to create the LTD product',
+            ltd_slug=ltd_slug)
+        if event['slack_username'] is not None:
+            await post_message(
+                text="Something went wrong setting up _LSST the Docs._ I will "
+                     "continue to configure the technote, but docs won't be "
+                     "available right away. Contact SQuaRE for help.",
+                channel=event['slack_channel'],
+                thread_ts=event['slack_thread_ts'],
+                logger=logger,
+                app=app
+            )
 
     try:
         await sync_travis_account(slug=slug, app=app, logger=logger)
---
+++ templatebotaide/lsstthedocs.py
@@ -0,0 +1,98 @@
+"""Workflows for LSST the Docs.
+"""
+
+__all__ = ('get_ltd_token', 'register_ltd_product')
+
+import aiohttp
+
+
+async def get_ltd_token(*, app, logger):
+    """Get an auth token for LSST the Docs.
+
+    Parameters
+    ----------
+    app : `aiohttp.web.Application`
+        The app instance.
+    logger
+        A `structlog` logger instance with bound context related to the
+        Kafka event.
+
+    Returns
+    -------
+    token : `str`
+        The auth token (use in the 'username' field of basic auth, without
+        a separate password).
+
+    Notes
+    -----
+    Auth is provided through these app-level configuration variables:
+
+    - ``templatebot-aide/ltdUsername``
+    - ``templatebot-aide/ltdPassword``
+    """
+    http_session = app['api.lsst.codes/httpSession']
+    url = 'https://keeper.lsst.codes/token'
+    auth = aiohttp.BasicAuth(
+        app['templatebot-aide/ltdUsername'],
+        password=app['templatebot-aide/ltdPassword'])
+    async with http_session.get(url, auth=auth) as response:
+        response.raise_for_status()
+        data = await response.json()
+    return data['token']
+
+
+async def register_ltd_product(*, slug, title, github_repo, app, logger):
+    """Register a new product on LSST the Docs.
+
+    Parameters
+    ----------
+    slug : `str`
+        The *slug* is the sub-domain component of the lsst.io domain.
+    title : `str`
+        The product's title.
+    github_repo : `str`
+        The URL of the product's source repository.
+    app : `aiohttp.web.Application`
+        The app instance.
+    logger
+        A `structlog` logger instance with bound context related to the
+        Kafka event.
+
+    Returns
+    -------
+    data : `dict`
+        The product resource, see
+        https://ltd-keeper.lsst.io/products.html#get--products-(slug)
+
+    Notes
+    -----
+    Authentication is done through application configuration variables. See
+    `get_ltd_token`.
+    """
+    data = {
+        'title': title,
+        'slug': slug,
+        'doc_repo': github_repo,
+        'bucket_name': 'lsst-the-docs',
+        'root_domain': 'lsst.io',
+        'root_fastly_domain': 'n.global-ssl.fastly.net',
+    }
+
+    http_session = app['api.lsst.codes/httpSession']
+    url = 'https://keeper.lsst.codes/products/'
+    logger.debug(
+        'Registering product on LTD',
+        url=url,
+        payload=data)
+    token = await get_ltd_token(app=app, logger=logger)
+    auth = aiohttp.BasicAuth(token)
+    async with http_session.post(url, json=data, auth=auth) as response:
+        response.raise_for_status()
+        product_url = response.headers['Location']
+
+    # Get data about the product
+    async with http_session.get(product_url, auth=auth) as response:
+        response.raise_for_status()
+        product_data = await response.json()
+
+    return product_data
