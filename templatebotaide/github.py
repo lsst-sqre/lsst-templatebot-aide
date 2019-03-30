df2eda3376f19e713987c0f18c04274cfaffaf3e
Jonathan Sick <jsick@lsst.org>
2019-03-29 18:49:42 -0700

    Add GitHub helper for get authed user
    This function is useful for getting information about the bot account on
    GitHub, which is great for using the bot itself has a committer to the
    GitHub repo.

--- templatebotaide/github.py
+++ templatebotaide/github.py
@@ -1,7 +1,7 @@
 """Workflows for GitHub operations common to many handlers.
 """
 
-__all__ = ('create_repo',)
+__all__ = ('create_repo', 'get_authenticated_user')
 
 
 async def create_repo(homepage=None, description=None, *, org_name, repo_name,
@@ -51,4 +51,29 @@ async def create_repo(homepage=None, description=None, *, org_name, repo_name,
         '/orgs{/org_name}/repos',
         url_vars={'org_name': org_name},
         data=data)
+    return response
+
+
+async def get_authenticated_user(*, app, logger):
+    """Get information about the authenticated GitHub user.
+
+    This function wraps the `GET /user
+    <https://developer.github.com/v3/users/#get-the-authenticated-user>`_
+    method.
+
+    Parameters
+    ----------
+    app : `aiohttp.web.Application`
+        The app instance.
+    logger
+        A `structlog` logger instance with bound context related to the
+        Kafka event.
+
+    Returns
+    -------
+    response : `dict`
+        The parsed JSON response body from GitHub.
+    """
+    ghclient = app['templatebot-aide/gidgethub']
+    response = await ghclient.getitem('/user')
     return response
